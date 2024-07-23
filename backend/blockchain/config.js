cce1b1201cc31de85de6f00144b5375fddc32ca6
Benson Mugure <bensonm@stormguidance.com>
2024-07-23 20:32:50 +0400

    update infra project id

--- backend/blockchain/config.js
+++ backend/blockchain/config.js
@@ -1,6 +1,6 @@
 const Web3 = require('web3');
 
-const web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID'));
+const web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/d83ca82ca706430eb2c16878fd99287f'));
 
 const contractABI = []; // Add your contract ABI here
 const contractAddress = '0xYourSmartContractAddress';
