ea206bb934bad7dbef0580e30133c38309385847
Benson Mugure <b.mugure@alustudent.com>
2024-07-24 07:10:03 +0400

    add smart contracts

--- backend/blockchain/tokenize.js
+++ backend/blockchain/tokenize.js
@@ -1,22 +1,26 @@
+require('dotenv').config();
 const { web3, contract } = require('./config');
+
+const accountAddress = process.env.ACCOUNT_ADDRESS;
+const privateKey = process.env.PRIVATE_KEY;
 
 const createProjectTokens = async (projectId, targetAmount, smallestTokenAmount, startDate, endDate) => {
   const numberOfTokens = Math.ceil(targetAmount / smallestTokenAmount / 10) * 10;
 
   const tx = contract.methods.createProjectTokens(projectId, numberOfTokens, startDate, endDate);
-  const gas = await tx.estimateGas({ from: '0xYourAccountAddress' });
+  const gas = await tx.estimateGas({ from: accountAddress });
   const data = tx.encodeABI();
-  const nonce = await web3.eth.getTransactionCount('0xYourAccountAddress');
+  const nonce = await web3.eth.getTransactionCount(accountAddress);
 
   const signedTx = await web3.eth.accounts.signTransaction(
     {
-      to: contractAddress,
+      to: contract.options.address,
       data,
       gas,
       nonce,
       chainId: web3.utils.toHex(1),
     },
-    'YOUR_PRIVATE_KEY'
+    privateKey
   );
 
   return web3.eth.sendSignedTransaction(signedTx.rawTransaction);
@@ -24,19 +28,19 @@ const createProjectTokens = async (projectId, targetAmount, smallestTokenAmount,
 
 const distributeFunds = async (projectId) => {
   const tx = contract.methods.distributeFunds(projectId);
-  const gas = await tx.estimateGas({ from: '0xYourAccountAddress' });
+  const gas = await tx.estimateGas({ from: accountAddress });
   const data = tx.encodeABI();
-  const nonce = await web3.eth.getTransactionCount('0xYourAccountAddress');
+  const nonce = await web3.eth.getTransactionCount(accountAddress);
 
   const signedTx = await web3.eth.accounts.signTransaction(
     {
-      to: contractAddress,
+      to: contract.options.address,
       data,
       gas,
       nonce,
       chainId: web3.utils.toHex(1),
     },
-    'YOUR_PRIVATE_KEY'
+    privateKey
   );
 
   return web3.eth.sendSignedTransaction(signedTx.rawTransaction);
